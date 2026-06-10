7c29b2edeb89babb73e2edb4ba7c502e0198d07b
chai-wan <dskkusano@gmail.com>
2026-06-10 22:28:13 +0900

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/**
+ * eBayの出品URLから数値のItemIDを取り出す。
+ * 例: https://www.ebay.com/itm/123456789012
+ *     https://www.ebay.com/itm/Some-Title/123456789012
+ *     https://www.ebay.com/itm/123456789012?hash=...
+ * 取り出せなければ null。
+ */
+export function extractEbayItemId(ebayUrl?: string | null): string | null {
+  if (!ebayUrl || typeof ebayUrl !== 'string') return null
+  const m = ebayUrl.match(/\/itm\/(?:[^/?#]+\/)?(\d{9,15})/)
+  if (m) return m[1]
+  const nums = ebayUrl.match(/\d{9,15}/g)
+  if (nums && nums.length) return nums.sort((a, b) => b.length - a.length)[0]
+  return null
+}
